bbd7344051982f12d3c8d29914dd8c929e9d064b
Artem Andreenko <miolini@gmail.com>
2016-01-28 02:09:06 +0300

    http api server

--- core/core.go
+++ core/core.go
@@ -4,21 +4,21 @@ import "fmt"
 import "github.com/labstack/echo"
 
 // EchoReplyJson reply with json standart structure with filed response
-func EchoReplyJson(ctx *echo.Context, v interface{}) {
-	ctx.JSON(200, map[string]interface{}{"response": v})
+func EchoReplyJson(ctx *echo.Context, v interface{}) error {
+	return ctx.JSON(200, map[string]interface{}{"response": v})
 }
 
 // EchoReplyJsonError reply with json standart error structure
-func EchoReplyJsonError(ctx *echo.Context, err interface{}) {
-	ctx.JSON(400, map[string]interface{}{"error": fmt.Sprintf("%s", err)})
+func EchoReplyJsonError(ctx *echo.Context, err interface{}) error {
+	return ctx.JSON(400, map[string]interface{}{"error": fmt.Sprintf("%s", err)})
 }
 
-func EchoJsonCheckErrorMW () echo.MiddlewareFunc {
+func EchoJsonCheckErrorMW() echo.MiddlewareFunc {
 	return func(h echo.HandlerFunc) echo.HandlerFunc {
 		return func(c *echo.Context) error {
 			err := h(c)
 			if err != nil {
-				EchoReplyJsonError(c, err)
+				return EchoReplyJsonError(c, err)
 			}
 			return nil
 		}
--- httpapi/common/app.go
+++ httpapi/common/app.go
@@ -1,54 +1,33 @@
 package common
 
 import (
-	"bytes"
 	"log"
-	"io"
-	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/miolini/bankgo/core"
 	"encoding/json"
 	"fmt"
+	"github.com/miolini/bankgo/rpc/client"
 )
 
-type App struct {
-	router *echo.Echo
-}
-
 type BalanceEntry struct {
-	UserID int64 `json:"UserID"`
+	UserID int64
 	Value  int64
 }
 
-type BalanceMap map[int64]int64
-
-func (bm BalanceMap) MarshalJson() ([]byte, error) {
-	var err error
-	buf := &bytes.Buffer{}
-	_, err = io.WriteString(buf, "{")
-	if err != nil {
-		return nil, err
-	}
-	for balanceId, balanceValue := range bm {
-		io.WriteString(buf, "  \"")
-		io.WriteString(buf, strconv.FormatInt(balanceId, 10))
-		io.WriteString(buf, "\": ")
-		io.WriteString(buf, strconv.FormatInt(balanceValue, 10))
-		io.WriteString(buf, ", \n")
-	}
-	buf.Truncate(buf.Len() - 3)
-	io.WriteString(buf, "\n")
-	_, err = io.WriteString(buf, "}")
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
+type App struct {
+	router *echo.Echo
+	client *client.BalanceStorageClient
 }
 
-func (app *App) Init() error {
+func (app *App) Init(rpcAddr string) error {
 	log.Printf("httpapi init")
+	client, err := client.Connect(rpcAddr)
+	if err != nil {
+		return err
+	}
+	app.client = client
 	app.router = echo.New()
 	app.router.Use(middleware.Logger(), middleware.Recover())
 	app.router.Use(core.EchoJsonCheckErrorMW())
@@ -62,19 +41,11 @@ func (app *App) Run(addr string) {
 }
 
 func (app *App) handleGetBalances(ctx *echo.Context) error {
-	balances, err := app.requestBalances()
+	records, err := app.client.AllBalances()
 	if err != nil {
 		return err
 	}
-	log.Printf("balances: %#v", balances)
-	data, err := balances.MarshalJson()
-	if err != nil {
-		return err
-	}
-	log.Printf("data: %s", data)
-	ctx.Response().Header().Add("Content-Type", "application/json")
-	ctx.Response().WriteHeader(200)
-	ctx.Response().Write(data)
+	core.EchoReplyJson(ctx, records)
 	return nil
 }
 
@@ -82,15 +53,11 @@ func (app *App) handlePostTransaction(ctx *echo.Context) error {
 	var balance BalanceEntry
 	err := json.NewDecoder(ctx.Request().Body).Decode(&balance)
 	if err != nil {
-		return fmt.Errorf("json parse error: %s", err)
+		return core.EchoReplyJsonError(ctx, fmt.Errorf("json parse error: %s", err))
 	}
-	ctx.JSON(200, balance)
-	return nil
-}
-
-func (app *App) requestBalances() (BalanceMap, error) {
-	return BalanceMap{
-		1: 100,
-		2: 40500,
-	}, nil
+	balance.Value, err = app.client.SetValue(balance.UserID, balance.Value)
+	if err != nil {
+		return core.EchoReplyJsonError(ctx, err)
+	}
+	return core.EchoReplyJson(ctx, balance)
 }
